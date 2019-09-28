d9e1eadd0e9c37466836c29b669c17f7446f26af
Singh <sumit.2.singh.ext@nokia.com>
2019-09-28 14:02:47 +0530

    6th Module - 3rd PArty Components

--- DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-constructor(private http: HttpClient) { }
+constructor(private http: HttpClient) {}
 
 login(model: any){
 return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -16,6 +19,7 @@ return this.http.post(this.baseUrl + 'login', model).pipe(
     const user = response;
     if (user) {
       localStorage.setItem('token', user.token);
+      this.decodedToken = this.jwtHelper.decodeToken(user.token);
     }
   })
 );
@@ -24,4 +28,10 @@ return this.http.post(this.baseUrl + 'login', model).pipe(
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
 }
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
+}
+
 }
--- DatingApp/DatingApp.API/Controllers/AuthController.cs
+++ DatingApp/DatingApp.API/Controllers/AuthController.cs
@@ -46,7 +46,6 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
 
-            throw new Exception("Computer says no!");
 ;            var userFromRepo = await _repo.Login(userForLoginDto.Username, userForLoginDto.Password);
 
             if (userFromRepo == null)
